1b998fd0c5d1a37a9e1a143008a7990eb315db8a
Milan Kovac <82383873+fws-milan@users.noreply.github.com>
2022-05-12 19:55:29 +0200

    Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ const port = process.env.PORT || 3000;
 require('./config/db')
 
 app.use(bodyParser.json());
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(userRouter);
 app.use(plotRouter);
 app.use(treeRouter);
